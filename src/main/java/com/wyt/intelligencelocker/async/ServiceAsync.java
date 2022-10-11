7109f27874800c582480e2a3c41be6bc12192e33
WeiYouting <99525651+WeiYouting@users.noreply.github.com>
2022-10-11 15:31:17 +0800

    Update ServiceAsync.java

--- src/main/java/com/wyt/intelligencelocker/async/ServiceAsync.java
+++ src/main/java/com/wyt/intelligencelocker/async/ServiceAsync.java
@@ -3,6 +3,10 @@ package com.wyt.intelligencelocker.async;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Component;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import javax.annotation.Resource;
 
 /**
  * @Author WeiYouting
@@ -14,10 +18,30 @@ import org.springframework.stereotype.Component;
 @Component
 @Slf4j
 public class ServiceAsync {
+    
+    @Resource
+    private JavaMailSender javaMailSender;
+
+    @Value("${email.from}")
+    private String from;
+
+    @Value("${email.to}")
+    private String to;
+
+    private String subject = "【智慧储柜】";
+
+    private String context = "您的验证码是:";
+
 
     // 使用线程池异步发送验证码
     @Async("taskExecutor")
     public void sendSMSCode(String code) {
+        SimpleMailMessage simpleMailMessage = new SimpleMailMessage();
+        simpleMailMessage.setFrom(from + "(智慧储柜项目组)");
+        simpleMailMessage.setTo(to);
+        simpleMailMessage.setSubject(subject);
+        simpleMailMessage.setText(context + code);
+        javaMailSender.send(simpleMailMessage);
         log.info("短信验证码发送成功：" + code);
     }
 
